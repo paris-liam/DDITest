a56824d8321880c0965c2b371557b85a6a14abef
paris-liam <lparis@fordham.edu>
2018-08-31 01:19:51 -0400

    size

--- src/components/Solution.js
+++ src/components/Solution.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Title = styled.div`
-  font-size: 1rem;
+  font-size: 1.5rem;
+  padding-bottom:10px;
 `;
 const Body = styled.div`
-  font-size: .75rem;
+  font-size: 1rem;
   color:white;
+  width:70%;
+  margin: 0 auto 0 auto;
 `;
 const Icon = styled.div`
-  font-size: 2rem;
+  font-size: 3.25rem;
 `;
 
 const Solution = solution => (
