a6a55c89d6e1448839c47c4efb01bc029abe01b7
paris-liam <lparis@fordham.edu>
2018-10-07 11:02:10 -0400

    fixed event issue

--- src/pages/Section179.js
+++ src/pages/Section179.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { colorsAndFont } from '../style/style';
+
 const PageGrid = styled.div`
     display:grid;
     height:88vh;
@@ -13,7 +14,7 @@ const PageGrid = styled.div`
         text-align:center;
         font-weight:bolder;
     }
-`
+`;
 const CalculatorGrid = styled.div`
     display:grid;
     grid-template-columns:1fr 1fr;
@@ -45,103 +46,103 @@ const Final = styled.div`
     display:grid;
     grid-template-columns:auto auto;
     grid-template-rows: 1fr 1fr;
-`
+`;
 
-class Section179 extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            taxBrack:.1,
-            purchaseAmount: 1,
-            deducation: 0,
-            bonus:'  -',
-            firstYearDeduction:0,
-            CashSavings:0,
-            LoweredCost:0,
-        }
-        this.calculator = this.calculator.bind(this);
-        this.checkAmount = this.checkAmount.bind(this);
-        this.preventEnter = this.preventEnter.bind(this);
+class Section179 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      taxBrack: 0.1,
+      purchaseAmount: 1,
+      deducation: 0,
+      bonus: '  -',
+      firstYearDeduction: 0,
+      CashSavings: 0,
+      LoweredCost: 0,
+    };
+    this.calculator = this.calculator.bind(this);
+    this.checkAmount = this.checkAmount.bind(this);
+    this.preventEnter = this.preventEnter.bind(this);
+  }
+  preventEnter(event) {
+    if (event.which == 13) {
+      event.preventDefault();
     }
-    preventEnter(event){
-        if(event.which == 13){
-            event.preventDefault();
-        }
+  }
+  checkAmount(value) {
+    if (value > 2500000) {
+      value = 2500000;
+    } else if (value < 0 || value == NaN) {
+      value = 0;
     }
-    checkAmount(value){
-        if(value > 2500000){
-            value = 2500000;
-        }
-        else if(value < 0 || value == NaN ){
-            value = 0;
-        }
-        return value;
+    return value;
+  }
+  calculator(event) {
+    event.preventDefault();
+    if (event.target.name == 'amount') {
+      event.target.value = this.checkAmount(event.target.value);
     }
-    calculator(event) {
-        event.preventDefault();
-            if(event.target.name == 'amount'){
-                event.target.value = this.checkAmount(event.target.value);
-            }
-            let taxBrack = document.getElementById('taxBrack').value;
-            let purchaseAmount = parseInt(document.getElementById('purchaseAmount').value);
-            let deduction = (purchaseAmount >= 1000000 ? (1000000):(purchaseAmount))
-            let bonus = (deduction >= 1000000 ? (purchaseAmount - 1000000):('-'));
-            let firstYearDeduction = deduction;
-            let CashSavings = purchaseAmount * taxBrack;
-            let LoweredCost = purchaseAmount - CashSavings;
-            this.setState({
-                taxBrack: taxBrack,
-                purchaseAmount: purchaseAmount,
-                deducation:deduction.toLocaleString(undefined, {maximumFractionDigits:2}),
-                bonus:bonus.toLocaleString(undefined, {maximumFractionDigits:2}),
-                firstYearDeduction:firstYearDeduction.toLocaleString(undefined, {maximumFractionDigits:2}),
-                CashSavings:CashSavings.toLocaleString(undefined, {maximumFractionDigits:2}),
-                LoweredCost:LoweredCost.toLocaleString(undefined, {maximumFractionDigits:2}),
-            });
-            this.forceUpdate();
-    }
-    render(){ return(
-        <PageGrid>
-            <h1>Section179 Calculator</h1>
-        <CalculatorGrid>
-            <Labels>
-                <div>Tax Bracket:</div>
-                <div>Equipment Purchase Amount:</div>
-                <div>Section 179 Deduction</div>
-                <div><p>Bonus Depreciation</p><p>(100% in 2018)</p></div>
-                <div>Normal First Year Depreciation:</div>
-                <div>Total First Year Deduction</div>
-                <div>Cash Savings on your Purchase</div>
-                <div>
-                <p>Lowered Cost of Equipment:</p>
-                <p>(After Tax Savings)</p>
-            </div>
+    const taxBrack = document.getElementById('taxBrack').value;
+    const purchaseAmount = parseInt(document.getElementById('purchaseAmount').value);
+    const deduction = (purchaseAmount >= 1000000 ? (1000000) : (purchaseAmount));
+    const bonus = (deduction >= 1000000 ? (purchaseAmount - 1000000) : ('-'));
+    const firstYearDeduction = deduction;
+    const CashSavings = purchaseAmount * taxBrack;
+    const LoweredCost = purchaseAmount - CashSavings;
+    this.setState({
+      taxBrack,
+      purchaseAmount,
+      deducation: deduction.toLocaleString(undefined, { maximumFractionDigits: 2 }),
+      bonus: bonus.toLocaleString(undefined, { maximumFractionDigits: 2 }),
+      firstYearDeduction: firstYearDeduction.toLocaleString(undefined, { maximumFractionDigits: 2 }),
+      CashSavings: CashSavings.toLocaleString(undefined, { maximumFractionDigits: 2 }),
+      LoweredCost: LoweredCost.toLocaleString(undefined, { maximumFractionDigits: 2 }),
+    });
+    this.forceUpdate();
+  }
+  render() {
+ return (
+      <PageGrid>
+          <h1>Section179 Calculator</h1>
+          <CalculatorGrid>
+          <Labels>
+              <div>Tax Bracket:</div>
+              <div>Equipment Purchase Amount:</div>
+              <div>Section 179 Deduction</div>
+              <div><p>Bonus Depreciation</p><p>(100% in 2018)</p></div>
+              <div>Normal First Year Depreciation:</div>
+              <div>Total First Year Deduction</div>
+              <div>Cash Savings on your Purchase</div>
+              <div>
+                  <p>Lowered Cost of Equipment:</p>
+                  <p>(After Tax Savings)</p>
+                </div>
             </Labels>
-            <InputGrid onSubmit={event.preventDefault()}>
-                <select name='tax' id='taxBrack'  onChange={this.calculator}>
-                    <option value='.1'>10%</option>
-                    <option value='.12'>12%</option>
-                    <option value='.21'>21%</option>
-                    <option value='.22'>22%</option>
-                    <option value='.24'>24%</option>
-                    <option value='.32'>32%</option>
-                    <option value='.35' selected>35%</option>
-                    <option value='.37'>37%</option>
+          <InputGrid onSubmit={(e) => { e.preventDefault(); }}>
+              <select name="tax" id="taxBrack" onChange={this.calculator}>
+                  <option value=".1">10%</option>
+                  <option value=".12">12%</option>
+                  <option value=".21">21%</option>
+                  <option value=".22">22%</option>
+                  <option value=".24">24%</option>
+                  <option value=".32">32%</option>
+                  <option value=".35" selected>35%</option>
+                  <option value=".37">37%</option>
                 </select>
-                <input id='purchaseAmount' name='amount' style={{display:'block'}} type='number' onChange={this.calculator} onKeyPress={this.preventEnter}></input>
-                <div>${this.state.deducation}</div>
-                <div>${this.state.bonus}</div>
-                <div>$0</div>
-                <div>${this.state.firstYearDeduction}</div>
-                <div>${this.state.CashSavings}</div>
-                <div>
+              <input id="purchaseAmount" name="amount" style={{ display: 'block' }} type="number" onChange={this.calculator} onKeyPress={this.preventEnter} />
+              <div>${this.state.deducation}</div>
+              <div>${this.state.bonus}</div>
+              <div>$0</div>
+              <div>${this.state.firstYearDeduction}</div>
+              <div>${this.state.CashSavings}</div>
+              <div>
                 ${this.state.LoweredCost}
-            </div>
+                </div>
             </InputGrid>
-            <p>*Information provided is for illustrative purpose only and accuracy is not guaranteed.</p>
+          <p>*Information provided is for illustrative purpose only and accuracy is not guaranteed.</p>
         </CalculatorGrid>
-        </PageGrid>)
-    }
+        </PageGrid>);
+  }
 }
 
 export default Section179;
