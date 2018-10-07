88ce1c7c7210225af14aaaad71665a4eb2e9c7e2
paris-liam <lparis@fordham.edu>
2018-10-07 10:45:28 -0400

    updated calculator

--- src/pages/Section179.js
+++ src/pages/Section179.js
@@ -5,11 +5,13 @@ const PageGrid = styled.div`
     display:grid;
     height:88vh;
     grid-template-columns:auto;
-    grid-template-rows:auto auto;
+    grid-template-rows:1fr 10fr;
     background-color:${colorsAndFont.blue};
     color:white;
     h1{
+        margin-top:3vh;
         text-align:center;
+        font-weight:bolder;
     }
 `
 const CalculatorGrid = styled.div`
@@ -59,35 +61,44 @@ class Section179 extends React.Component{
         }
         this.calculator = this.calculator.bind(this);
         this.checkAmount = this.checkAmount.bind(this);
+        this.preventEnter = this.preventEnter.bind(this);
     }
-    checkAmount(event){
-        if(event.target.value > 2500000){
-            event.target.value = 2500000;
+    preventEnter(event){
+        if(event.which == 13){
+            event.preventDefault();
         }
+    }
+    checkAmount(value){
+        if(value > 2500000){
+            value = 2500000;
+        }
+        else if(value < 0 || value == NaN ){
+            value = 0;
+        }
+        return value;
     }
     calculator(event) {
-        if(event.target.name == 'amount'){
-            if(event.target.value > 2500000){
-                event.target.value = 2500000
+        event.preventDefault();
+            if(event.target.name == 'amount'){
+                event.target.value = this.checkAmount(event.target.value);
             }
-        }
-        let taxBrack = document.getElementById('taxBrack').value;
-        let purchaseAmount = document.getElementById('purchaseAmount').value;
-        let deduction = (purchaseAmount >= 1000000 ? (1000000):(purchaseAmount))
-        let bonus = (deduction >= 1000000 ? (purchaseAmount - 1000000):('-'));
-        let firstYearDeduction = deduction;
-        let CashSavings = purchaseAmount * taxBrack;
-        let LoweredCost = purchaseAmount - CashSavings;
-        this.setState({
-            taxBrack: taxBrack,
-            purchaseAmount: purchaseAmount,
-            deducation:deduction,
-            bonus:bonus,
-            firstYearDeduction:firstYearDeduction,
-            CashSavings:CashSavings,
-            LoweredCost:LoweredCost,
-        });
-        this.forceUpdate();
+            let taxBrack = document.getElementById('taxBrack').value;
+            let purchaseAmount = parseInt(document.getElementById('purchaseAmount').value);
+            let deduction = (purchaseAmount >= 1000000 ? (1000000):(purchaseAmount))
+            let bonus = (deduction >= 1000000 ? (purchaseAmount - 1000000):('-'));
+            let firstYearDeduction = deduction;
+            let CashSavings = purchaseAmount * taxBrack;
+            let LoweredCost = purchaseAmount - CashSavings;
+            this.setState({
+                taxBrack: taxBrack,
+                purchaseAmount: purchaseAmount,
+                deducation:deduction.toLocaleString(undefined, {maximumFractionDigits:2}),
+                bonus:bonus.toLocaleString(undefined, {maximumFractionDigits:2}),
+                firstYearDeduction:firstYearDeduction.toLocaleString(undefined, {maximumFractionDigits:2}),
+                CashSavings:CashSavings.toLocaleString(undefined, {maximumFractionDigits:2}),
+                LoweredCost:LoweredCost.toLocaleString(undefined, {maximumFractionDigits:2}),
+            });
+            this.forceUpdate();
     }
     render(){ return(
         <PageGrid>
@@ -102,22 +113,22 @@ class Section179 extends React.Component{
                 <div>Total First Year Deduction</div>
                 <div>Cash Savings on your Purchase</div>
                 <div>
-                <p>Lowered Cost of Equiptment:</p>
+                <p>Lowered Cost of Equipment:</p>
                 <p>(After Tax Savings)</p>
             </div>
             </Labels>
-            <InputGrid>
-                <select name='tax' id='taxBrack'  onInput={this.calculator}>
+            <InputGrid onSubmit={event.preventDefault()}>
+                <select name='tax' id='taxBrack'  onChange={this.calculator}>
                     <option value='.1'>10%</option>
                     <option value='.12'>12%</option>
                     <option value='.21'>21%</option>
                     <option value='.22'>22%</option>
                     <option value='.24'>24%</option>
                     <option value='.32'>32%</option>
-                    <option value='.35'>35%</option>
+                    <option value='.35' selected>35%</option>
                     <option value='.37'>37%</option>
                 </select>
-                <input id='purchaseAmount' name='amount' style={{display:'block'}} type='number' onInput={this.calculator}></input>
+                <input id='purchaseAmount' name='amount' style={{display:'block'}} type='number' onChange={this.calculator} onKeyPress={this.preventEnter}></input>
                 <div>${this.state.deducation}</div>
                 <div>${this.state.bonus}</div>
                 <div>$0</div>
